97d955ddcbd71c24341a81a6056ee8553ef7b734
Geovanni <temporale47@gmail.com>
2024-10-29 16:40:54 -0600

    Agregado de pytyhon datos

---
+++ Python.py/HolaMundo.py
@@ -0,0 +1,18 @@
+# Esto es un comentario VE MÁS RÁPIDO
+"""
+ESTO ES UN COMENTARIO MULTILINEA
+PUEDO PONER VARIAS LINEAS DE COMENTARIO
+"""
+# HOLA MUNDO EN PYTHON
+print ("Hola mundo 😆")
+
+
+# Esto es una variable
+saludo = "Hola mundo!!!!!"
+print(saludo) #aqui se imprime la variable 
+
+#esta es una variable|
+saludo = "Adios" #Aqui lo que hace es cambiar lo qu estaba en la variable, no cres otra
+print(saludo)
+saludo = 5
+print(saludo)
